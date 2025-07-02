f73a0a337772116cf6555242c026a532a41bdb84
Romanvark <jrromero@uamv.edu.ni>
2025-07-01 21:14:32 -0600

    contrasena

--- app.py
+++ app.py
@@ -1,4 +1,31 @@
 from funciones.menu import main
+import pwinput
+
+def pedir_credenciales():
+    try:
+        with open("credenciales.txt", "r") as f:
+            linea = f.readline().strip()
+            usuario_correcto, contraseña_correcta = linea.split(",")
+    except FileNotFoundError:
+        print("Archivo de credenciales no encontrado.")
+        return False
+
+    intentos = 3
+    while intentos > 0:
+        usuario = input("Ingrese el usuario: ")
+        contraseña = pwinput.pwinput("Ingrese la contraseña: ")
+
+        if usuario == usuario_correcto and contraseña == contraseña_correcta:
+            print("\nAcceso concedido.\n")
+            return True
+        else:
+            intentos -= 1
+            print(f"Credenciales incorrectas. Intentos restantes: {intentos}")
+
+    print("Acceso denegado. Cerrando el programa.")
+    return False
+
 
 if __name__ == "__main__":
-    main()
+    if pedir_credenciales():
+        main()
